85b5630e2d3cb8b2934c342021273a559a9d00ce
mexavas <mexavas.me@gmail.com>
2023-11-28 08:30:58 +0900

    파이어베이스,구글로 로그인, 부트스트랩
    파이어베이스 설정, 구글로 로그인 기능, 부트스트랩설정, Navbar 기능

---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAVojixBMFd2_j6wQXM_xUIQEjsdnIWmEI",
+    authDomain: "nowboard-id.firebaseapp.com",
+    projectId: "nowboard-id",
+    storageBucket: "nowboard-id.appspot.com",
+    messagingSenderId: "410590825436",
+    appId: "1:410590825436:web:838a1253a52ec4bbef5d19",
+    measurementId: "G-PPPW2L1YPM"
+  };
