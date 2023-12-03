ce1155112b53fb4af6b588ee0110e3dd770d9a12
mexavas <mexavas.me@gmail.com>
2023-12-03 18:52:46 +0900

    프론트엔드 기본 프로제트 설계
    프론트엔드 기본 프로제트 설계 & 로그인 및 회원가입 UI 완성

--- firebase.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyAVojixBMFd2_j6wQXM_xUIQEjsdnIWmEI",
-    authDomain: "nowboard-id.firebaseapp.com",
-    projectId: "nowboard-id",
-    storageBucket: "nowboard-id.appspot.com",
-    messagingSenderId: "410590825436",
-    appId: "1:410590825436:web:838a1253a52ec4bbef5d19",
-    measurementId: "G-PPPW2L1YPM"
-  };
