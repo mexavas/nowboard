fce68d02891f844d2670e5dfb90f682153e7d17f
mexavas <mexavas.me@gmail.com>
2023-12-16 17:00:42 +0900

    올리기 페이지 추가 완료

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -5,18 +5,22 @@ const auth = firebase.auth();
         const signInLink = document.getElementById("sign-in-link");
 
         if (user) {
+
             // User is logged in
             const userName = user.displayName;
-            signInLink.innerText = userName + '님👋';
-            signInLink.href = "#";
+            signInLink.innerText = '포스터 올리기👆';
+            signInLink.href = "./create.html";
             // logout if you user click on this button
             // create a logout anchor tag in footer with id called footer and create that button with id="logout" and  poster-link class
             const logout = document.getElementById("logout");
             logout.innerText = "로그아웃";
             logout.addEventListener('click', function() {
-                auth.signOut();
-                location.reload();
-            });
+                var userResponse = window.confirm("로그아웃 하시겠습니까?");
+                if (userResponse) {
+                    auth.signOut();
+                    location.reload();
+                }
+            })
         } else {
             // User is not logged in
             signInLink.innerText = "로그인하세요";
